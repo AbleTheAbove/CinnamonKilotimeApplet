2e781e5fe06d450debf5bb0644d573b6696c4834
elfein <patlymlatun@gmail.com>
2021-11-08 16:35:48 -0800

    Add precision adjustment.

--- kilotime@ablecorp.us/applet.js
+++ kilotime@ablecorp.us/applet.js
@@ -3,58 +3,59 @@ const Util = imports.misc.util;
 const Settings = imports.ui.settings;
 
 class Kilotime extends Applet.TextApplet {
-    constructor(orientation, panel_height, instance_id) {
-        super(orientation, panel_height, instance_id);
+   constructor(orientation, panel_height, instance_id) {
+      super(orientation, panel_height, instance_id);
 
-        this.setAllowedLayout(Applet.AllowedLayout.BOTH);
+      this.setAllowedLayout(Applet.AllowedLayout.BOTH);
 
-        try {
-            this.orientation = orientation;
-            this.settings = new Settings.AppletSettings(this, "kilotime@ablecorp.us", this.instance_id);
-            this.settings.bind("use-decimal-point", "use_decimal_point", this._onSettingsChanged);
-            this.interval_id = 0;
-        } catch (e) {
-            global.logError(e);
-        }
-    }
-    _onSettingsChanged() {
-        this._updateClock();
-    }
-    _updateClock() {
-        let now = new Date(),
-            then = new Date(
-                now.getFullYear(),
-                now.getMonth(),
-                now.getDate(),
-                0, 0, 0),
-            diff = now.getTime() - then.getTime();
-        if (this.use_decimal_point) {
-            let kiloseconds = Math.floor(diff / 1000) / 1000;
-            this.set_applet_label(("0" + kiloseconds.toFixed(3)).slice(-6) + "k");
-        } else {
-            let kiloseconds = Math.floor(diff / 1000000),
-                seconds = Math.floor((diff / 1000)) % 1000;
-            this.set_applet_label(kiloseconds + "K " + ("000" + seconds).slice(-3) + "S");
-        }
-    }
-    on_applet_added_to_panel() {
-        this._updateClock();
+      try {
+         this.orientation = orientation;
+         this.settings = new Settings.AppletSettings(this, "kilotime@ablecorp.us", this.instance_id);
+         this.settings.bind("use-decimal-point", "use_decimal_point", this._onSettingsChanged);
+         this.settings.bind("clock-precision", "clock_precision", this._onSettingsChanged);
+         this.interval_id = 0;
+      } catch (e) {
+         global.logError(e);
+      }
+   }
+   _onSettingsChanged() {
+      this._updateClock();
+   }
+   _updateClock() {
+      let now = new Date(),
+         then = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            0, 0, 0),
+         diff = now.getTime() - then.getTime();
+      if (this.use_decimal_point) {
+         let kiloseconds = diff / 1000000;
+         this.set_applet_label(("0" + kiloseconds.toFixed(this.clock_precision)).slice(-((3 - (this.clock_precision == 0)) + this.clock_precision)) + "k");
+      } else {
+         let kiloseconds = Math.floor(diff / 1000000),
+            seconds = Math.floor((diff / 1000)) % 1000;
+         this.set_applet_label(kiloseconds + "K " + ("000" + seconds).slice(-3) + "S");
+      }
+   }
+   on_applet_added_to_panel() {
+      this._updateClock();
 
-        if (this.interval_id == 0) {
-            this.interval_id = setInterval(() => {
-                this._updateClock();
-            }, 100);
-        }
+      if (this.interval_id == 0) {
+         this.interval_id = setInterval(() => {
+            this._updateClock();
+         }, 100);
+      }
 
-    }
-    on_applet_removed_from_panel() {
-        if (this.interval_id > 0) {
-            clearInterval(this.interval_id);
-            this.interval_id = 0;
-        }
-    }
+   }
+   on_applet_removed_from_panel() {
+      if (this.interval_id > 0) {
+         clearInterval(this.interval_id);
+         this.interval_id = 0;
+      }
+   }
 }
 
 function main(metadata, orientation, panel_height, instance_id) {
-    return new Kilotime(orientation, panel_height, instance_id);
+   return new Kilotime(orientation, panel_height, instance_id);
 }
